c8faeb3850cd16d73cb9c688e8a29fd0806f843f
Emmanuele Somma <exedre@gmail.com>
2018-03-05 16:20:50 +0100

    Lezione 1

---
+++ go.sh
@@ -0,0 +1,5 @@
+scrapy shell http://quotes.toscrape.com/random
+>>> response.css('small.author::text').extract_first()
+>>> response.css('span.text::text').extract_first()
+>>> response.css('a.tag::text').extract()
+
