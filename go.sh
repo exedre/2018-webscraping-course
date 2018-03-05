83ad9a628e170b9fc0d760533700845118b78ab6
Emmanuele Somma <exedre@gmail.com>
2018-03-05 16:42:16 +0100

    lezione 02 02

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
-scrapy genspider quotes toscrape.com
+scrapy runspider quotes.py
 
--- quotes.py
+++ quotes.py
@@ -5,8 +5,14 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['toscrape.com']
-    start_urls = ['http://toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/random']
 
     def parse(self, response):
-        self.log('I just visited: ' + response.url)
-    
+        self.log('I just visited: ' + response.url)    
+        for quote in response.css('div.quote'):
+            item = {
+                'author_name': quote.css('small.author::text').extract_first(),
+                'text': quote.css('span.text::text').extract_first(),
+                'tags': quote.css('a.tag::text').extract(),
+            }
+            yield item
